6202b6d6c2a39c62da599e15677cdd58a42956c5
Quang <quang.pham5@onemount.com>
2024-03-09 14:59:29 +0700

    add vault vm template

--- startup_script_vault.sh
+++ startup_script_vault.sh
@@ -27,3 +27,5 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update && sudo apt install vault
+
+sudo systemctl enable vault
