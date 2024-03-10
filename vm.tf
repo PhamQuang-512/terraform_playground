c953f0366315fecb7f55aa347d193a3da7534a31
Quang <quang.pham5@onemount.com>
2024-03-10 17:27:35 +0700

    update vault vm

--- vm.tf
+++ vm.tf
@@ -3,21 +3,22 @@ locals {
   vault_instances = [
     "vault-1",
     "vault-2",
-    "vault-3",
+    # "vault-3",
   ]
 }
 
 resource "google_compute_instance" "vault_instances" {
-  for_each = toset(local.vault_instances)
-  name                     = join("-", ["quangpham5", each.value])
+  for_each     = toset(local.vault_instances)
+  name         = join("-", ["quangpham5", each.value])
   machine_type = "e2-medium"
+  tags         = ["vault", "http-server", "https-server"]
 
   boot_disk {
     initialize_params {
-      size = "10"
+      size  = "10"
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
-      type = "pd-standard"
-    } 
+      type  = "pd-standard"
+    }
   }
   network_interface {
     network    = google_compute_network.quangpham5.self_link
@@ -29,8 +30,9 @@ resource "google_compute_instance" "vault_instances" {
 
   metadata_startup_script = file("./startup_script_vault.sh")
   scheduling {
-    provisioning_model = "SPOT"
-    preemptible        = true
-    automatic_restart  = false
+    provisioning_model          = "SPOT"
+    preemptible                 = true
+    automatic_restart           = false
+    instance_termination_action = "STOP"
   }
 }
