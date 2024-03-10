2cb11369b6bef2c0fed2e08d6c520939c0bf5169
Quang <quang.pham5@onemount.com>
2024-03-11 01:11:27 +0700

    init vault

--- provider.tf
+++ provider.tf
@@ -4,6 +4,12 @@ terraform {
       source = "hashicorp/google"
       version = "~>5.11.0"
     }
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.25.0"
+    }
+    # key: AoqJKxYmiVftAyeyEjNF5gzEyRtBPGQx9/BAgEPnFJw=
+    # token: hvs.utJw9vBnTHM00fdPv6VNme6f
   }
   backend "gcs" {
     bucket  = "quangpham5-bucket"
@@ -27,4 +33,10 @@ provider "google" {
   region = var.region
   zone = var.zone
   access_token = data.google_service_account_access_token.default.access_token
+}
+
+
+provider "vault" {
+  token = "hvs.utJw9vBnTHM00fdPv6VNme6f"
+  skip_tls_verify = true
 }
